d74e27d6c8d2bdf6c74051ae23ad27cc70d5f00e
Tahir Hayat <Tahir.Hayat@Bentley.com>
2025-10-31 12:40:32 +0500

    autocapture is set to true

--- src/amplitude.ts
+++ src/amplitude.ts
@@ -3,5 +3,7 @@ import { plugin as engagementPlugin } from '@amplitude/engagement-browser';
 
 const AMPLITUDE_API_KEY = 'fc2b67baa715261b5314c27096785943';
 
-amplitude.init(AMPLITUDE_API_KEY);
+amplitude.init(AMPLITUDE_API_KEY, {
+  autocapture: true,
+});
 amplitude.add(engagementPlugin());
